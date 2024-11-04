8994ffde262fba3218dd457df0dc16b9e91693d9
Priyankat124 <155417207+Priyankat124@users.noreply.github.com>
2024-11-04 13:57:54 +0530

    changes made

--- dist/script.js
+++ dist/script.js
@@ -4,14 +4,5 @@ function toggleMenu() {
     menu.classList.toggle("flex");
 }
 
-const sr = ScrollReveal({
-    distance: '50px',
-    duration: 1000,
-    easing: 'ease-out',
-    origin: 'bottom',
-    delay: 100,
-    reset: true 
-});
 
-sr.reveal('.all_section');
 
